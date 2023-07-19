a22d5df6413b630df259451ba25a3218ad3c0240
Alexander Grooff <alexandergrooff@gmail.com>
2023-07-20 01:18:34 +0200

    docs: root command docs

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2023 Alexander Grooff <alexandergrooff@gmail.com>
 */
 package cmd
 
@@ -16,16 +16,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "boxes",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Manage multiple machines",
+	Long: `Run commands in a parallel manner on multiple machines.
+This is typically useful for ad-hoc commands without too much fuzz.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -45,10 +38,6 @@ func init() {
 	// will be global for your application.
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.boxes.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
--- cmd/run.go
+++ cmd/run.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2023 Alexander Grooff <alexandergrooff@gmail.com>
 */
 package cmd
 
